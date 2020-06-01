b999ce7063e55f6f415823b34eaefd85c5b9a346
enrique-bit <62971062+enrique-bit@users.noreply.github.com>
2020-05-31 19:03:39 -0500

    Add files via upload
    App dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { PlanesComponent } from './componentes/planes/planes.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { CrearProyectoComponent } from './componentes/crear-proyecto/crear-proyecto.component';
+import { CrearPlanComponent } from './componentes/crear-plan/crear-plan.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+	LayoutComponent,
+    PlanesComponent,
+    ProyectosComponent,
+    CrearProyectoComponent,
+    CrearPlanComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+	FormsModule,
+	HttpClientModule,
+	Ng2SmartTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
