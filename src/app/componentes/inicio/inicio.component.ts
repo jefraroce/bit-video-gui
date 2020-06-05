94248ed18d038dbcdf8e95dabbdb47dbb6c94bc3
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2020-06-05 16:38:50 -0500

    Agregada carga dinamica de proyectos.

--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProyectosService } from 'src/app/servicios/proyectos.service';
+const swal = require('sweetalert');
 
 @Component({
   selector: 'app-inicio',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.scss']
 })
 export class InicioComponent implements OnInit {
+  proyectos: Array<any>;
 
-  constructor() { }
+  constructor(private proyectosService: ProyectosService) { }
 
   ngOnInit(): void {
+    this.cargarProyectos();
+  }
+
+  cargarProyectos() {
+    this.proyectosService.traerProyectos()
+      .subscribe((proyectosCargados: Array<any>) => {
+        this.proyectos = proyectosCargados;
+        console.log(this.proyectos)
+      },
+      (error) => {
+        swal('Error', error, 'error');
+        console.error('Error cargando los proyectos: ', error);
+      })
   }
 
 }
